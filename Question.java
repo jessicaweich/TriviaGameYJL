63c23387086a33c659a60065e6affe12ad0cc185
heathl1 <148890833+heathl1@users.noreply.github.com>
2024-12-13 13:26:17 -0600

    Update README.md

---
+++ Question.java
@@ -0,0 +1,9 @@
+public class Question {
+    private String question;
+    private String answer1;
+    private String answer2;
+    private String answer3;
+    private String answer4;
+    private int correctIndex;
+
+}
---
+++ TriviaGame.java
@@ -0,0 +1,5 @@
+public class TriviaGame {
+    public static void main(String[] args) {
+        //main
+    }
+}
